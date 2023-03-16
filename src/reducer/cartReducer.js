cabed68d0dfa1ed0ac590bf1d4cae3d6b552a52c
Diego Gonzalez <gonzalezbusnego@gmail.com>
2023-03-16 11:17:29 -0400

    move reducer to a custom hook

---
+++ src/reducer/cartReducer.js
@@ -0,0 +1,33 @@
+export const initialState = []
+
+// reduce el estado a partir de una acción
+export const reducer = (state, action) => {
+  const { type: actionType, payload: actionPayload } = action
+  switch (actionType) {
+    case 'ADD_TO_CART': {
+      const { id } = actionPayload
+      const productInCartIndex = state.findIndex((item) => item.id === id)
+
+      if (productInCartIndex >= 0) {
+        const newState = structuredClone(state)
+        newState[productInCartIndex].quantity += 1
+        return newState
+      }
+      return [
+        ...state,
+        {
+          ...actionPayload,
+          quantity: 1
+        }
+      ]
+    }
+    case 'REMOVE_FROM_CART': {
+      const { id } = actionPayload
+      return state.filter((item) => item.id !== id)
+    }
+    case 'CLEAR_CART': {
+      return initialState
+    }
+  }
+  return state
+}
